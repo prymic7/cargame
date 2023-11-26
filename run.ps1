839223fa2cace69b9f162ac5ee48f47fea764745
prymic7 <152082367+prymic7@users.noreply.github.com>
2023-11-26 22:17:23 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,5 @@
-g++ main.cpp -o game.exe -O1 -Wall -Wno-missing-braces -I includes/ -L lib/ -lraylib -lgdi32 -lwinmm -static-libgcc -static-libstdc++
+//Compiler code
+g++.exe main.cpp -o game.exe -O1 -Wall -Wno-missing-braces -I includes/ -L lib/ -lraylib -lgdi32 -lwinmm -static-libgcc -static-libstdc++
+
+//if this doenst work, try
+g++.exe main.cpp -o game.exe -O1 -Wall -Wno-missing-braces -I includes/ -L lib/ -lraylib -lgdi32 -lwinmm
